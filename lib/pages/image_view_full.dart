851a1738d98f970653e54ec329d8927998ed0660
Leta-D <letadeju.1278@gmail.com>
2025-05-04 18:57:36 +0300

    finished image full view page

--- lib/pages/image_view_full.dart
+++ lib/pages/image_view_full.dart
@@ -5,22 +5,34 @@ import 'package:artify/app_theme/app_colors.dart';
 import 'package:artify/network_control/network_object.dart';
 import 'package:artify/network_control/network_to_api.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ImageViewFull extends StatefulWidget {
   String imageUrl;
-  ImageViewFull(this.imageUrl);
+  ImageViewFull(this.imageUrl, {super.key});
   @override
   createState() => _ImageViewFullState(imageUrl);
 }
 
 class _ImageViewFullState extends State<ImageViewFull> {
   String imageUrl;
+  bool isVisible = false;
+  double posX = 0;
+  double posY = 0;
   _ImageViewFullState(this.imageUrl);
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.sizeOf(context);
+    if (posX == 0 && posY == 0 ||
+        posX < 0 ||
+        posY < 0 ||
+        posX > screenSize.width - 30 ||
+        posY > screenSize.height - 30) {
+      posX = screenSize.width / 1.23;
+      posY = screenSize.height / 1.35;
+    }
     return Stack(
       alignment: Alignment.center,
       children: [
@@ -37,7 +49,9 @@ class _ImageViewFullState extends State<ImageViewFull> {
           padding: const EdgeInsets.all(15.0),
           child: CachedNetworkImage(
             imageUrl: imageUrl,
-            fit: BoxFit.fill,
+            scale: 2,
+            height: screenSize.height - screenSize.height / 7.4,
+            fit: BoxFit.cover,
             progressIndicatorBuilder: (context, url, progress) {
               return Center(
                 child: SizedBox(
@@ -53,7 +67,153 @@ class _ImageViewFullState extends State<ImageViewFull> {
             fadeInCurve: Curves.easeIn,
           ),
         ),
-        // InkWell(child: Container()),
+
+        Align(
+          alignment: Alignment.topLeft,
+          child: IconButton(
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(appBlack(0.5)),
+            ),
+            padding: EdgeInsets.all(8.0),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(CupertinoIcons.back, size: 30.0, color: appWhite(1)),
+          ),
+        ),
+        Positioned(
+          left: posX,
+          top: posY,
+          child: GestureDetector(
+            onPanUpdate: (details) {
+              setState(() {
+                posX += details.delta.dx;
+                posY += details.delta.dy;
+              });
+            },
+            child: FloatingActionButton(
+              backgroundColor: appBlack(1),
+              foregroundColor: appWhite(1),
+              onPressed: () {
+                print("Download");
+              },
+              child: Icon(CupertinoIcons.down_arrow),
+            ),
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            AnimatedOpacity(
+              opacity: isVisible ? 1 : 0,
+              duration: Duration(milliseconds: 900),
+              child: AnimatedSlide(
+                offset: isVisible ? Offset.zero : Offset(0, 1),
+                duration: Duration(milliseconds: 100),
+                child: Stack(
+                  children: [
+                    Opacity(
+                      opacity: 0.4,
+                      child: Container(
+                        height: screenSize.height / 5,
+                        width: screenSize.width / 2.7,
+                        decoration: BoxDecoration(
+                          gradient: buttonGradient,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border(
+                            top: BorderSide(
+                              color: const Color.fromARGB(255, 247, 62, 6),
+                              width: 3,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenSize.height / 5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Lock Screen",
+                              style: TextStyle(
+                                color: appWhite(1),
+                                fontSize: screenSize.height / 45,
+                              ),
+                            ),
+                          ),
+                          // Spacer(),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Home Screen",
+                              style: TextStyle(
+                                color: appWhite(1),
+                                fontSize: screenSize.height / 45,
+                              ),
+                            ),
+                          ),
+                          // Spacer(),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Both",
+                              style: TextStyle(
+                                color: appWhite(1),
+                                fontSize: screenSize.height / 45,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: screenSize.width / 2.7,
+                height: screenSize.height / 18.3,
+                child: Material(
+                  type: MaterialType.canvas,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      print("Set wallpeper");
+                      setState(() {
+                        isVisible = !isVisible;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: buttonGradient,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          !isVisible ? "Set as" : "Cancel",
+                          style: TextStyle(
+                            fontSize: screenSize.height / 36.6,
+                            color: appWhite(1),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }

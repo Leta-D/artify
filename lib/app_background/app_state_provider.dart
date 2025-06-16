baa5ef11f9339222fe6463edad18e552ef6be762
Leta-D <letadeju.1278@gmail.com>
2025-06-16 19:15:47 +0300

    first commit from aaa12

--- lib/app_background/app_state_provider.dart
+++ lib/app_background/app_state_provider.dart
@@ -10,7 +10,7 @@ class AppStateProvider extends ChangeNotifier {
   }
 
   // for main page
-  int _mainBottomNavIndex = 1;
+  int _mainBottomNavIndex = 0;
   int get mainBottomNavIndex => _mainBottomNavIndex;
   void changeMainBottomNavIndex(int index) {
     _mainBottomNavIndex = index;
